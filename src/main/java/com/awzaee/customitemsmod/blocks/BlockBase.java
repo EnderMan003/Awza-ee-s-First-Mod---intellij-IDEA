23f0e02f898d0946add573f637666e07097c562c
Dark Assassin <minecraftawzaee@outlook.comj>
2019-03-14 15:19:29 +0500

    Learning to mod

--- src/main/java/com/awzaee/customitemsmod/blocks/BlockBase.java
+++ src/main/java/com/awzaee/customitemsmod/blocks/BlockBase.java
@@ -6,11 +6,20 @@ import com.awzaee.customitemsmod.init.ModItems;
 import com.awzaee.customitemsmod.util.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockBase extends Block implements IHasModel
 {
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+
     public BlockBase(String name, Material material)
     {
         super(material);
@@ -20,6 +29,47 @@ public class BlockBase extends Block implements IHasModel
 
         ModBlocks.BLOCKS.add (this);
         ModItems.ITEMS.add (new ItemBlock(this).setRegistryName(this.getRegistryName ()));
+    }
+
+    @Override
+    public BlockStateContainer getBlockState() {
+        return super.getBlockState();
+    }
+    @Override
+    public boolean isOpaqueCube (IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube (IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public IBlockState getStateForPlacement (World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        IBlockState state = super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer);
+        return state.withProperty(FACING, placer.getHorizontalFacing());
+    }
+
+    @Override
+    public int getMetaFromState (IBlockState state)
+    {
+        return state.getValue(FACING).getHorizontalIndex();
+    }
+
+    @Override
+    public IBlockState getStateFromMeta ( int meta)
+    {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState ()
+    {
+        return new BlockStateContainer(this, FACING);
     }
 
     @Override
--- src/main/java/com/awzaee/customitemsmod/blocks/BlockTable.java
+++ src/main/java/com/awzaee/customitemsmod/blocks/BlockTable.java
@@ -3,8 +3,11 @@ package com.awzaee.customitemsmod.blocks;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -14,60 +17,47 @@ import net.minecraft.world.World;
 
 public class BlockTable extends BlockBase {
 
-    public BlockTable(String name) {
-        super(name, Material.WOOD);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+
+        public BlockTable(String name) {
+            super(name, Material.WOOD);
+            this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+        }
+
+        @Override
+        public BlockStateContainer getBlockState()
+        {
+            return super.getBlockState();
+        }
+
+        @Override
+        public IBlockState getStateForPlacement (World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+        {
+            IBlockState state = super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer);
+            return state.withProperty(FACING, placer.getHorizontalFacing());
+        }
+
+        @Override
+        public int getMetaFromState (IBlockState state)
+        {
+            return state.getValue(FACING).getHorizontalIndex();
+        }
+
+        @Override
+        public IBlockState getStateFromMeta ( int meta)
+        {
+            return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+        }
+
+        @Override
+        protected BlockStateContainer createBlockState ()
+        {
+            return new BlockStateContainer(this, FACING);
+        }
+
+        @Override
+        public IBlockState withRotation(IBlockState state, Rotation rot)
+        {
+            return state.withProperty(BlockHorizontal.FACING, rot.rotate(state.getValue(BlockHorizontal.FACING)));
+        }
     }
-
-    public static final PropertyDirection FACING = BlockHorizontal.FACING;
-
-
-    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
-    {
-        this.setDefaultFacing(worldIn, pos, state);
-    }
-
-   private void setDefaultFacing(World worldIn, BlockPos pos, IBlockState state)
-   {
-       if (!worldIn.isRemote)
-       {
-           IBlockState iblockstate = worldIn.getBlockState(pos.north());
-           IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
-           IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
-           IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
-           EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
-
-           if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock())
-           {
-               enumfacing = EnumFacing.SOUTH;
-           }
-           else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock())
-           {
-               enumfacing = EnumFacing.NORTH;
-           }
-           else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock())
-           {
-               enumfacing = EnumFacing.EAST;
-           }
-           else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock())
-           {
-               enumfacing = EnumFacing.WEST;
-           }
-
-           worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
-       }
-   }
-
-
-    @Override
-    public boolean isOpaqueCube(IBlockState state)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean isFullCube(IBlockState state)
-    {
-        return false;
-    }
-}
